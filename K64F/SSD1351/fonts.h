41c81df030e3e6f801f7e1dea15ef62c288caaae
LuisPerez406 <62678791+LuisPerez406@users.noreply.github.com>
2020-12-04 20:05:53 -0600

    Create fonts.h
    INCOMPLETE VERSION

---
+++ K64F/SSD1351/fonts.h
@@ -0,0 +1,22 @@
+/*
+ * fonts.h
+ *
+ *  Created on: Nov 22, 2020
+ *      Author: Luis Pérez
+ *     Version: 1.0
+ *   
+ *     Description:
+ */
+
+#ifndef FONTS_H_
+#define FONTS_H_
+
+	#define BLACK		0x0000
+	#define BLUE		0x001F
+	#define GREEN		0x07E0
+	#define BROWN		0x79E0
+	#define RED			0xF800
+	#define WHITE		0xFFFF
+
+
+#endif
